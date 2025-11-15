940b81f21e9b37e3f15bf44e5e6b37fa4d02f959
Adam Thomas <thomasadam83@hotmail.com>
2025-11-14 23:43:22 -0500

    Move bar.js to html_footer.php

--- colours/list.php
+++ colours/list.php
@@ -19,7 +19,12 @@ include('../templates/message.php');
 
     <h1>BrickMMO Colour Palette</h1>
 
-    <input class="w3-input w3-border w3-margin" type="text" placeholder="Seach using RGB or colour name" style="max-width: 400px; display: inline-block;" id="search">
+    <input 
+        class="w3-input w3-border w3-margin" 
+        type="text" 
+        placeholder="Seach using RGB or colour name" 
+        style="max-width: 400px; display: inline-block;" 
+        id="search">
 
 </div>
 

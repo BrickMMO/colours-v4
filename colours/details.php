7e7e3a854142dd2db36c18f8e8339fe32d255708
Adam Thomas <thomasadam83@hotmail.com>
2025-11-25 23:35:13 -0500

    Update colours/details.php

--- colours/details.php
+++ colours/details.php
@@ -25,30 +25,33 @@ $record = mysqli_fetch_assoc($result);
 
 ?>
 
-<main>   
-    <div class="w3-center">
-        <h1><?=$record['name']?></h1>
+
+<div class="w3-center">
+    <h1><?=$record['name']?></h1>
+</div>
+
+<hr>
+
+<div>
+    <div style="display: inline-block; position: relative; width: 100%;  height: 150px; margin-bottom: 16px;">
+        <div style="background-color: #<?=$record['rgb']?>; width: 100%; height: 100%;"></div>
+        <?php if($record['is_trans'] == 'yes'): ?>
+            <span style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none; background: url('https://cdn.brickmmo.com/images@1.0.0/trans-checkers.png') repeat; background-position: center; opacity: 0.5; border-radius: 8px;"></span>
+        <?php endif; ?>
     </div>
-    <hr>
-    <div class="w3-container w3-padding">
-        <div style="display: inline-block; position: relative; width: 100%;  height: 150px; margin-bottom: 16px;">
-            <div style="background-color: #<?=$record['rgb']?>; width: 100%; height: 100%;"></div>
-            <?php if($record['is_trans'] == 'yes'): ?>
-                <span style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none; background: url('https://cdn.brickmmo.com/images@1.0.0/trans-checkers.png') repeat; background-position: center; opacity: 0.5; border-radius: 8px;"></span>
-            <?php endif; ?>
-        </div>
-        <p><strong>Name:</strong> <?=$record['name']?></p>
-        <p><strong>RGB:</strong> #<?=$record['rgb']?></p>
-        <p><strong>Transparent:</strong> <?=$record['is_trans'] == 'yes' ? 'Yes' : 'No'?></p>
-        <p><strong>Externals:</strong> <?=$record['externals'] ?: '-'?></p>
-        <p><strong>ID:</strong> <?=$record['id']?></p>
-    </div>
-    <hr>
-    <a href="/q" class="w3-button w3-white w3-border">
-        <i class="fa-solid fa-caret-left fa-padding-right"></i>
-        Back to Colour List
-    </a>
-</main>
+    <p><strong>Name:</strong> <?=$record['name']?></p>
+    <p><strong>RGB:</strong> #<?=$record['rgb']?></p>
+    <p><strong>Transparent:</strong> <?=$record['is_trans'] == 'yes' ? 'Yes' : 'No'?></p>
+    <p><strong>Externals:</strong> <?=$record['externals'] ?: '-'?></p>
+    <p><strong>ID:</strong> <?=$record['id']?></p>
+</div>
+
+<hr>
+
+<a href="/q" class="w3-button w3-white w3-border">
+    <i class="fa-solid fa-caret-left fa-padding-right"></i>
+    Back to Colour List
+</a>
 
 <?php
 
